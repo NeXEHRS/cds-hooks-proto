d85a481818ce3252774ec27d53c3c5bc2ce370e4
Nakamura-Norihiko <150097324+Nakamura-Norihiko@users.noreply.github.com>
2023-12-28 09:54:13 +0900

    Delete cds-hooks-client directory

--- cds-hooks-client/ソースコード一式/src/main/java/jp/cpcnexehrs/cds/hooks/client/service/CdsHooksClientService.java
+++
@@ -1,9 +0,0 @@
-package jp.cpcnexehrs.cds.hooks.client.service;
-
-public interface CdsHooksClientService {
-
-    String sendDiscoveryRequest(String url);
-
-    String sendHooksRequest(String url, String requestBody);
-
-}
