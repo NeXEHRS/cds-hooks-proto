059af0c8ba8b328504b9d097e6be5af6cd9f07e6
Nakamura-Norihiko <150097324+Nakamura-Norihiko@users.noreply.github.com>
2023-12-28 14:13:15 +0900

    Add files via upload

---
+++ cds-hooks-client/ソースコード一式/src/main/java/jp/cpcnexehrs/cds/hooks/client/form/DiscoveryForm.java
@@ -0,0 +1,15 @@
+package jp.cpcnexehrs.cds.hooks.client.form;
+
+public class DiscoveryForm {
+
+    private String responseBody;
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+
+    public void setResponseBody(String responseBody) {
+        this.responseBody = responseBody;
+    }
+
+}
